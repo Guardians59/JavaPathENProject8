e2a8a612837c216d4e5a82f69bcdb38b5fd6f27c
Dylann Vandenbussche <vandenbussche.dylann@gmail.com>
2022-06-07 20:08:54 +0200

    Start Update for Feign

--- ms-rewardcentral/src/main/java/ms/rewardcentral/controller/RewardCentralController.java
+++ ms-rewardcentral/src/main/java/ms/rewardcentral/controller/RewardCentralController.java
@@ -5,10 +5,13 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ms.rewardcentral.controller.exception.RewardException;
+import ms.rewardcentral.model.FeignRewardPointsModel;
 import ms.rewardcentral.service.IRewardCentralService;
 
 @RestController
@@ -24,7 +27,16 @@ public class RewardCentralController {
 	if(!result.containsKey("reward")) {
 	    throw new RewardException("An error occurred while searching for if reward available");
 	}
-	
+	return result;
+    }
+    
+    @PostMapping("/getRewardPoints")
+    public HashMap<String, Integer> getRewardPointsProxy(@RequestBody FeignRewardPointsModel feignRewardPointsModel) {
+	HashMap<String, Integer> result = new HashMap<>();
+	result = rewardCentralService.getRewardPoints(feignRewardPointsModel.getAttractionId(), feignRewardPointsModel.getUserId());
+	if(!result.containsKey("reward")) {
+	    throw new RewardException("An error occurred while searching for if reward available");
+	}
 	return result;
     }
     
