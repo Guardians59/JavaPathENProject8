a24e4cd4d1367965fa43a2f75b6a2278574944c9
Dylann Vandenbussche <vandenbussche.dylann@gmail.com>
2022-07-21 11:03:55 +0200

    Update jacoco report

---
+++ TourGuide/src/test/java/tourGuide/ApplicationTest.java
@@ -0,0 +1,18 @@
+package tourGuide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+    @Test
+    public void contextTest() {
+	Application.main(new String[] {});
+    }
+
+}
