ec0b41fa587633268ea37d2b73d2d8cca28e99b8
Dylann Vandenbussche <vandenbussche.dylann@gmail.com>
2022-08-04 12:17:48 +0200

    Application Test

---
+++ ms-gpsutil/src/test/java/ms/gpsutil/ApplicationTest.java
@@ -0,0 +1,18 @@
+package ms.gpsutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+    @Test
+    public void contextTest() {
+	Application.main(new String[] {});
+    }
+
+}
